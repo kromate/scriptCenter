6be2bcb2093689e2d8eefb426344e1738db29b2d
kromate <akpananthony33@gmail.com>
2022-08-15 18:42:53 +0100

    feat: header

--- src/components/pages/landing/HeroSection.vue
+++ src/components/pages/landing/HeroSection.vue
@@ -1,13 +1,24 @@
 <template>
-	<div>
-		Hero Section
+	<div class="w-full h-auto bg md:p-20 p-6 flex flex-col justify-center items-center text-center gap-4 ">
+		<h1 class="text-5xl font-bold ">
+			Find and Share scripts
+		</h1>
+		<p class="text-xl font-light w-full max-w-lg">
+			Contribute to the open source community with other students. Learn, collaborate, and share knowledge to build your portfolio and grow your network.
+		</p>
 	</div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
+.bg {
+	background-image: linear-gradient(
+			rgba(255, 255, 255, 0.35),
+			rgba(255, 255, 255, 0.35)
+		),
+		url('@/assets/images/bg.png');
+		background-position: 100% 50%;
 
+}
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
 <template>
 	<div>
 		<LayoutNavBar />
+		<LazyPagesLandingHeroSection />
 	</div>
 </template>
 
