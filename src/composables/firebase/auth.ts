ba45a6589fe92ff5f87b931ce570e37ea051540d
kromate <akpananthony33@gmail.com>
2022-08-22 19:10:40 +0100

    feat: restructure the firebase folder

--- src/composables/firebase/auth.ts
+++ src/composables/firebase/auth.ts
@@ -5,53 +5,38 @@ import {
   onAuthStateChanged,
   signOut
 } from 'firebase/auth'
-import { useAlert, useLoading } from '../composables/useNotification'
-import { useUser } from '../composables/useGlobals'
-import { app } from './init'
 
-const { openAlert } = useAlert()
-const { openLoading, closeLoading } = useLoading()
-const { clearUser, saveUser } = useUser()
+import { app } from './init'
 
 const auth = getAuth(app)
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    saveUser(user)
-  } else {
-    clearUser()
-    // User is signed out
-    // ...
-  }
-})
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     saveUser(user)
+//   } else {
+//     clearUser()
+//   }
+// })
 
 const provider = new GoogleAuthProvider()
 
 export const googleAuth = () => {
-  openLoading('Logging you in... 🤩')
   signInWithPopup(auth, provider)
     .then((result) => {
-      closeLoading()
       const user = result.user
-      saveUser(user)
-      openAlert('You have successfully signed in 🥳')
+      return user
     })
     .catch((error) => {
-      closeLoading()
-      openAlert(`Oops seems something went wrong 😕 : ${error.message}`)
+return error
     })
 }
 
 export const signOutUser = () => {
-  openLoading('Signing You out...😗')
   signOut(auth)
     .then(() => {
-      clearUser()
       location.reload()
-      closeLoading()
     })
     .catch((error) => {
-      closeLoading()
-      openAlert(`Oops seems something went wrong 😕 : ${error.message}`)
+      return error
     })
 }
--- src/composables/firebase/firestore.ts
+++ src/composables/firebase/firestore.ts
@@ -4,27 +4,16 @@ import {
   doc,
   setDoc,
   deleteDoc,
-  collection,
-  query,
-  where,
-  getDocs,
-  getDoc,
-  onSnapshot
+  getDoc
 } from 'firebase/firestore'
-import { ref } from '@vue/reactivity'
 import { app } from './init'
 
 export const db = getFirestore(app)
-
-const result = [] as any
-const pageBlockRef = collection(db, 'pageBlocks')
-export const pageTitle = ref('')
 
 export const saveToFirestore = async (collection:string, data:any) => {
     const id = uuidv4()
     await setDoc(doc(db, collection, id), data)
 }
-
 export const getSingleFirestoreDocument = async (collection:string, id:string) => {
   const singlepageBlockRef = doc(db, collection, id)
   const docSnap = await getDoc(singlepageBlockRef)
@@ -33,4 +22,8 @@ export const getSingleFirestoreDocument = async (collection:string, id:string) =
   } else {
     return null
   }
+}
+
+export const deleteFirestoreDocument = async (collection:string, id:string) => {
+  await deleteDoc(doc(db, collection, id))
 }
