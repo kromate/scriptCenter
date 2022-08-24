aa8804c9b585d0bd850c43824c726798635eb7e7
kromate <akpananthony33@gmail.com>
2022-08-24 04:35:55 +0100

    feat: started the editor aspect

--- src/pages/editor/index.vue
+++ src/pages/editor/index.vue
@@ -8,7 +8,9 @@
 <script setup lang="ts">
 import Nav from '@/components/editor/Nav.vue'
 import Main from '@/components/editor/Main.vue'
-console.log('test')
+const id = useRoute().query.id
+const type = useRoute().query.type
+
 </script>
 
 <style>
